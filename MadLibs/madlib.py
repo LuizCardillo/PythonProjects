d19159c5c69fbc3546508ec1f921ffe99bad9117
Luiz Cardillo <cardillo.luiz@gmail.com>
2024-09-10 17:03:05 -0300

    Adicionado projeto MadLibs

---
+++ MadLibs/madlib.py
@@ -0,0 +1,10 @@
+#Código simples para a receber entradas do usuário e concatenar utilizando f-string.
+
+
+adjetivo = input("Adjetivo: ")
+adjetivo2 = input("Segundo Adjetivo: ")
+sonho = input("Sonho: ")
+
+frase = f"Eu sou uma pessoa {adjetivo} e faço coisas {adjetivo2} e um dia gostaria muito de fazer {sonho}!"
+
+print(frase)
